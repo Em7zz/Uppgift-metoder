a903d845bc868cb8a70d07b89119b41b678e34f0
Emil Gräs <emgr02002@utb.vaxjo.se>
2019-10-21 12:03:09 +0200

    Vi har börjat med metoder för att räkna ut en cirkels omkrets och area.

---
+++ Uppgift Metoder/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+            Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
+
+        }
+        static double CirkelArea(double r)
+        {
+            double R = r * r * 3.14;
+            return R;
+
+        }
+        static double CirkelOmkrets(double r)
+        {
+            double R = (r + r) * 3.14;
+            return R;
+
+       }
+    }
+}
